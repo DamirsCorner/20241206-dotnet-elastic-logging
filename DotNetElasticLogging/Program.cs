a6ef72f914ae5adc91b296dd9aee9afabb15cba6
Damir Arh <damir.arh@gmail.com>
2024-12-01 17:11:33 +0100

    Add project files.

---
+++ DotNetElasticLogging/Program.cs
@@ -0,0 +1,28 @@
+using Elastic.Extensions.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddElasticsearch();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
